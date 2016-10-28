364649dabeb8033d22ca88d6316eb79e85298510
Iryna Ricker <Iryna.Ricker@webtrends.com>
2016-10-28 15:11:01 -0700

    reformatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,32 @@
-$(document).ready(function() {
-  $(".btn").click(function(event) {
- event.preventDefault();
+$(document).ready(function () {
+    $(".btn").click(function (event) {
+        event.preventDefault();
+        if($(".pingPongList").length > 0){
+            $(".pingPongList").remove();
+        }
+        var myArr = [];
+        var myPong = document.getElementById("Number").value;
+        var list = document.createElement('ul');
+        list.setAttribute("class", "pingPongList");
+        for (var i = 1; i <= myPong; i++) {
+            if (i % 15 === 0) {
+                myArr.push("pingpong")
+            }
+            else if (i % 3 === 0) {
+                myArr.push("ping");
+            }
+            else if (i % 5 === 0) {
+                myArr.push("pong")
+            }
+            else {
+                myArr.push(i);
+            }
+            var item = document.createElement('li');
+            item.appendChild(document.createTextNode(myArr[i - 1]));
+            list.appendChild(item);
+        }
 
-var myArr = [];
-var myPong = document.getElementById("Number").value;
-var list = document.createElement('ul');
-for (var i=1;i<=myPong;i++){
- if (i%15 === 0){
-myArr.push("pingpong")
-}
-else if(i%3 === 0){
-myArr.push("ping");
-}
-else if(i%5 ===0){
-myArr.push("pong")
-}
-else{
-myArr.push(i);
-}
- var item = document.createElement('li');
- item.appendChild(document.createTextNode(myArr[i-1]));
- list.appendChild(item);
-}
+        document.getElementById("Pong").appendChild(list);
 
-document.getElementById("Pong").appendChild(list);
-
- });
+    });
 });
