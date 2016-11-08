ceb38acf081c12c78bc9a401c5b59b7502dcda5a
vg <viktorgrom84@gmail.com>
2016-11-08 13:30:55 -0800

    BL and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,42 @@
+// Business logic
+
+var myArr = [];
+
+function pingpong(max){
+  for (var i = 1; i <= max; i++) {
+      if (i % 15 === 0) {
+          myArr.push("pingpong")
+      }
+      else if (i % 3 === 0) {
+          myArr.push("ping");
+      }
+      else if (i % 5 === 0) {
+          myArr.push("pong")
+      }
+      else {
+          myArr.push(i);
+      }
+
+  }
+}
 // UI logic
 
 $(document).ready(function () {
     $(".btn").click(function (event) {
         event.preventDefault();
-
+        myArr=[];
         if($(".pingPongList").length > 0){
             $(".pingPongList").remove();
         }
-        var myArr = [];
         var myPong = document.getElementById("Number").value;
         var list = document.createElement('ul');
 
        document.getElementById("Pong").appendChild(list);
-
-// Business logic
-
-
+       pingpong(myPong);
 
         list.setAttribute("class", "pingPongList");
-        for (var i = 1; i <= myPong; i++) {
-            if (i % 15 === 0) {
-                myArr.push("pingpong")
-            }
-            else if (i % 3 === 0) {
-                myArr.push("ping");
-            }
-            else if (i % 5 === 0) {
-                myArr.push("pong")
-            }
-            else {
-                myArr.push(i);
-            }
+        for (var i = 1; i <= myArr.length; i++) {
+
             var item = document.createElement('li');
             item.appendChild(document.createTextNode(myArr[i - 1]));
             list.appendChild(item);
